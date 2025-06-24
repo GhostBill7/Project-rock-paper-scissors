eb13da870cb3017d82bf2e636ed695dca5631038
GhostBill7 <194431184+GhostBill7@users.noreply.github.com>
2025-06-24 08:47:24 -0400

    Add user input via prompt with game choices
    Add three choices like rock, paper, and scissors that users can pick from via prompt

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ function getComputerChoice(){
     const random = Math.floor(Math.random() * 3) + 1;
 
     if(random === 1){
-        return "Rock"
+        return "Rock";
     }
     else if(random === 2){
-        return "Paper"
+        return "Paper";
     }
     else if(random === 3){
-        return "Scissors"
+        return "Scissors";
     }
     
+}
+
+function getHumanChoice(){
+    const choice = window.prompt("Rock, Paper, Scissors");
+
+    if(choice === "Rock"){
+        return "Rock";
+    }
+    else if(choice === "Paper"){
+        return "Paper";
+    }
+    else if(choice === "Scissors"){
+        return "Scissors";
+    }
 }
